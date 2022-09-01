3e4f6606573c40080a15265b094dabe014b738bd
Olad1109 <logos_b.9.4410y@mail.ru>
2022-09-01 19:23:50 +0400

    Working with Shapes

---
+++ src/main/java/com/training/exception/IllegalTest.java
@@ -0,0 +1,30 @@
+package main.java.com.training.exception;
+
+public class IllegalTest {
+
+    public static void main (String[] args) {
+        try {
+            AreaCheck.printResult(90);
+        } catch (Exception ex) {
+            ex.printStackTrace ();
+        }
+    }
+
+    static class IllegalAreaException extends Exception {
+        public IllegalAreaException (double x) {
+            System.err.println ("Значение не допустимо: " + x);
+            System.exit (0);
+        }
+    }
+
+    static class AreaCheck {
+        public static void printResult (double x) throws IllegalAreaException {
+            if (x >= 1) {
+                if (x <= 120) {
+                    System.out.println ("Значение допустимо: " + x);
+                } else throw new IllegalAreaException (x);
+            } else throw new IllegalAreaException (x);
+        }
+    }
+
+}
