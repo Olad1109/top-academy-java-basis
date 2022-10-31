506ef40fc4905a9f2fc0aa59aecf85ffe3c9eff6
Olad1109 <logos_b.9.4410y@mail.ru>
2022-10-31 15:20:53 +0400

    Multithreaded server tests

---
+++ src/main/java/com/training/client_server/Main.java
@@ -0,0 +1,29 @@
+package main.java.com.training.client_server;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    // private static ServerSocket server;
+
+    public static void main (String[] args) throws IOException, InterruptedException {
+
+        // запустим пул нитей в которых количество возможных нитей ограничено -
+        // 10-ю
+        ExecutorService exec = Executors.newFixedThreadPool(10);
+        int j = 0;
+
+        // Стартуем цикл, в котором с паузой в 10 миллисекунд стартуем Runnable.
+        // Клиенты, которые пишут какое-то количество сообщений.
+        while (j < 10) {
+            j ++;
+            exec.execute(new TestRunnableClientTester());
+            Thread.sleep(10);
+        }
+
+        // закрываем фабрику
+        exec.shutdown();
+    }
+}
